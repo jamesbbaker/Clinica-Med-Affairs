bd4edd4887408c70b87b5458a527b3c08ed3c191
vishal-hiehq <vishal.bais@hiehq.com>
2024-03-20 13:29:10 +0530

    refactor(login)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { Router, RouterProvider } from "react-router-dom";
-import router from "./Router";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
