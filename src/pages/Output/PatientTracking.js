95b15f11403f86419a0756cf075406b2047e5839
vishal-hiehq <vishal.bais@hiehq.com>
2024-04-12 20:33:36 +0530

    fix(patientTracking): update sankey chart 1 title

--- src/pages/Output/PatientTracking.js
+++ src/pages/Output/PatientTracking.js
@@ -5,7 +5,7 @@ const PatientTracking = () => {
     console.log("snake")
   return (
     <div className='px-12 py-10'>
-   <Sankey API={"sankey_data_6"} OPTIONS={{
+   <Sankey API={"sankey_data_6"} title={"Patient Transitions through HCP Specialities by Asthma Visit"} OPTIONS={{
     from: 'From_Specialty',
     to: 'To_Specialty',
     count: "Patient_Count"
