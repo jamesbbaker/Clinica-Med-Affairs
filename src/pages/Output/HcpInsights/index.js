7c4f868f869993a92b31611efd70b9ea4e67683f
vishal-hiehq <vishal.bais@hiehq.com>
2024-06-14 22:03:02 +0530

    add map colors

--- src/pages/Output/HcpInsights/index.js
+++ src/pages/Output/HcpInsights/index.js
@@ -56,7 +56,7 @@ const HcpInsight = () => {
     <div role="status" className="grid place-content-center h-[200px]">
       <svg
         aria-hidden="true"
-        class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -70,7 +70,7 @@ const HcpInsight = () => {
           fill="currentFill"
         />
       </svg>
-      <span class="sr-only">Loading...</span>
+      <span className="sr-only">Loading...</span>
     </div>
   );
 };
