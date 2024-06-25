c3a420ccdc063a1584fc5a6be6326970f391e440
vishal-hiehq <vishal.bais@hiehq.com>
2024-06-25 19:55:34 +0530

    add payer variation data scatter chart

--- src/pages/Output/PayerVariation/index.js
+++ src/pages/Output/PayerVariation/index.js
@@ -16,6 +16,7 @@ import Popup from "reactjs-popup";
 import CustomDropdown from "../../../components/CustomDropdown";
 import BarChartPopup from "../PatientOpportunityMapping/Popup";
 import { getLowestValue, highestValue } from "../../../utils/MathUtils";
+import PayerVariationBubbleChart from "./PayerVariationBubbleChart";
 
 const filters = [...Object.keys(selectLabels)];
 
@@ -26,12 +27,12 @@ const PayerVariation = () => {
   const [toggleFilter, setToggleFilter] = useState(filters[0]);
   const [values, setValues] = useState({
     min: 0,
-    max: 0
-  })
+    max: 0,
+  });
   const [treeDataById, setTreeDataById] = useState({});
-  const [data1, setData1] = useState(null)
+  const [data1, setData1] = useState(null);
   const [showModal, setShowModal] = useState(false);
-  const [loading , setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [modalDetails, setModalDetails] = useState({});
   const [sizeValueMap, setSizeValueMap] = useState({});
   const options1 = {
@@ -39,7 +40,7 @@ const PayerVariation = () => {
     minColor: "#00FF00",
     maxDepth: 0,
     maxPostDepth: 0,
-    midColor: "#FFA500",
+    midColor: "#fff",
     maxColor: "#FF0000",
     headerHeight: 15,
     fontColor: "black",
@@ -51,8 +52,7 @@ const PayerVariation = () => {
     useWeightedAverageForAggregation: true,
     showScale: true,
     generateTooltip: (_row, _size, value) => {
-    
-      let hcpValue = TreeData[_row + 1]
+      let hcpValue = TreeData[_row + 1];
 
       return `<div style="background:rgb(0 141 218);display: flex; align-items:center; flex-direction:column; color:#fff; padding:10px; border-style:solid, zIndex: 10"> 
     <div><strong>NAME</strong>:  ${hcpValue[0]}</div>
@@ -63,7 +63,7 @@ const PayerVariation = () => {
   };
   const options2 = {
     minColor: "#00FF00",
-    midColor: "#FFA500",
+    midColor: "#ddd",
     maxColor: "#FF0000",
     headerHeight: 15,
     fontColor: "black",
@@ -74,8 +74,7 @@ const PayerVariation = () => {
     },
     showScale: true,
     generateTooltip: (_row, _size, value) => {
-    
-      let hcpValue = TreeData[_row + 1]
+      let hcpValue = TreeData[_row + 1];
 
       return `<div style="background:rgb(0 141 218);display: flex; align-items:center; flex-direction:column; color:#fff; padding:10px; border-style:solid, zIndex: 10"> 
     <div><strong>NAME</strong>:  ${hcpValue[0]}</div>
@@ -106,17 +105,19 @@ const PayerVariation = () => {
         newObj[item.Item] = item;
         let newArr = [
           item.Item,
-          item.Parent && item.Parent.toLowerCase() == "global" ? "PAYER" : item.Parent,
+          item.Parent && item.Parent.toLowerCase() == "global"
+            ? "PAYER"
+            : item.Parent,
           item["Number of ICS-LABA Patients"],
           item[toggleFilter],
         ];
         treemapData.push(newArr);
       });
       setTreeDataById(newObj);
-    
+
       setTreeData(treemapData);
     }
-  }
+  };
 
   useEffect(() => {
     fetchData();
@@ -138,24 +139,21 @@ const PayerVariation = () => {
   };
 
   const closeModal = () => {
-    setLoading(true) 
+    setLoading(true);
     setTimeout(() => {
-        setLoading(false)
+      setLoading(false);
     }, 500);
     setShowModal(false);
   };
 
-  const fetchData = (
-    
-  ) => {
-
+  const fetchData = () => {
     let queryString = `payer_level_data?`; // Start with 'hcp_data?&'
 
     getDataStats(queryString, accessToken, refreshToken)
       .then((res) => {
         if (res) {
           let _data = JSON.parse(res.replaceAll("NaN", 0));
-          setRawData(_data.data)
+          setRawData(_data.data);
           if (_data) {
             let treemapData = [
               [
@@ -168,25 +166,33 @@ const PayerVariation = () => {
             ];
             let newObj = {};
             let data = _data.data;
-           let lowestValue =  getLowestValue(data, toggleFilter )
-           let _highestValue =  highestValue(data, toggleFilter )
-           setValues({
-            min: lowestValue,
-            max: _highestValue
-           })
+            let lowestValue = getLowestValue(data, toggleFilter);
+            let _highestValue = highestValue(data, toggleFilter);
+            setValues({
+              min: lowestValue,
+              max: _highestValue,
+            });
 
             data.map((item, index) => {
               newObj[item.Item] = item;
               let newArr = [
                 item.Item,
-                item.Parent &&  item.Parent.toLowerCase() == "global" ? "PAYER" : item.Parent,
+                item.Parent && item.Parent.toLowerCase() == "global"
+                  ? "PAYER"
+                  : item.Parent,
                 item["Number of ICS-LABA Patients"],
                 item[toggleFilter],
               ];
               treemapData.push(newArr);
             });
             setTreeDataById(newObj);
-          
+            let newArr = [];
+            treemapData.map((item) => {
+              if (!isNaN(item[3])) {
+                newArr.push(item[3]);
+              }
+            });
+
             setTreeData(treemapData);
           }
         }
@@ -227,36 +233,39 @@ const PayerVariation = () => {
       mapValue3: generateChartData(mapBarCharts.chart3),
       mapValue4: generateChartData(mapBarCharts.chart4),
       mapValue5: generateChartData(mapBarCharts.chart5),
-      ...data
+      ...data,
     });
   }
-
-
 
   return (
     <div className="w-full flex flex-col gap-4">
       {TreeData && !loading ? (
         <>
-         <div className="flex mb-6 items-center gap-8">
-              <CustomDropdown
+          <div className="flex mb-6 items-center gap-8">
+            <CustomDropdown
               showColors
-                labelClassName="mb-0"
-                className={"flex items-center"}
-                input={{
-                  label: "Select Unmet Needs",
-                  id: "unmet",
-                  options: filters.map((item) => ({ name: selectLabels[item], value: item })),
-                }}
-                handleSelect={(e) => handleToggleFilter(e)}
-                value={toggleFilter}
-              />
-            </div>
+              labelClassName="mb-0"
+              className={"flex items-center"}
+              input={{
+                label: "Select Unmet Needs",
+                id: "unmet",
+                options: filters.map((item) => ({
+                  name: selectLabels[item],
+                  value: item,
+                })),
+              }}
+              handleSelect={(e) => handleToggleFilter(e)}
+              value={toggleFilter}
+            />
+          </div>
 
           <TreeMap
-          values={values}
-          preventDrill={true}
+            values={values}
+            preventDrill={true}
             data={TreeData}
-            options={toggleFilter == filters[0] ? options1 : options2}
+            options={
+              toggleFilter == filters[0] ? { ...options1 } : { ...options2 }
+            }
             handleOpen={handleOpen}
           />
           <Popup
@@ -269,12 +278,27 @@ const PayerVariation = () => {
               <BarChartPopup closeModal={closeModal} payerData data1={data1} />
             )}
           </Popup>
+          <PayerVariationBubbleChart />
         </>
       ) : (
-        <div className="w-full h-[400px] grid place-content-center">
-          <div className="flex justify-center items-center h-24">
-            <div className="w-6 h-6 border-4 border-t-4 border-blue-500 rounded-full animate-spin"></div>
-          </div>
+        <div role="status" className="grid place-content-center h-[200px]">
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
         </div>
       )}
     </div>
