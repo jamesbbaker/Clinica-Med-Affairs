0d6cd2ae82f40e5845a5ae6b33606a748d2b48b5
vishal-hiehq <vishal.bais@hiehq.com>
2024-07-08 19:54:16 +0530

    add new help page with accordion

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,81 @@ import InputForm from "../../components/InputForm";
 import { useSelector } from "react-redux";
 import RealTimeBox from "../../components/RealTimeBox";
 import HCPTable from "../../components/HCPTable";
-import { sidebarRoutes } from "../../constants/appConstants.js";
+import { APP_ROUTES, APP_ROUTES_LABEL, sidebarRoutes } from "../../constants/appConstants.js";
+import { AiOutlineBarChart, AiOutlineDotChart, AiOutlineFundProjectionScreen, AiOutlineGlobal, AiOutlineInfo, AiOutlineProject, AiOutlineQuestionCircle, AiOutlineRise, AiOutlineTable } from "react-icons/ai";
+
+const NavigationMenu = [
+  {
+    name: APP_ROUTES_LABEL.patient_opportunity_mapping_and_strategy,
+    id: APP_ROUTES.patient_opportunity_mapping_and_strategy,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.patient_opportunity_mapping_and_strategy}`,
+    icon: () => <AiOutlineGlobal />,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu.",
+  },
+  {
+    name: APP_ROUTES_LABEL.eligible_patient_locator,
+    id: APP_ROUTES.eligible_patient_locator,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.eligible_patient_locator}`,
+    icon: () => <AiOutlineTable />,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu.",
+  },
+  {
+    name: APP_ROUTES_LABEL.institutional_variation,
+    id: APP_ROUTES.institutional_variation,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.institutional_variation}`,
+    icon: () => <AiOutlineProject />,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu."
+  },
+  {
+    name: APP_ROUTES_LABEL.payer_variation,
+    id: APP_ROUTES.payer_variation,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.payer_variation}`,
+    icon: () => <AiOutlineProject />,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu."
+  },
+  {
+    name: APP_ROUTES_LABEL.medical_affair_toolbox,
+    id: APP_ROUTES.medical_affair_toolbox,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.medical_affair_toolbox}`,
+      icon: () => <AiOutlineDotChart />,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu."
+  },
+  {
+    name: APP_ROUTES_LABEL.impact_tracking,
+    id: APP_ROUTES.impact_tracking,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.impact_tracking}`,
+    icon: () => <AiOutlineFundProjectionScreen/>,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu.",
+  },
+  {
+    name: APP_ROUTES_LABEL.patient_journey,
+    id: APP_ROUTES.patient_journey,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.patient_journey}`,
+    icon: () => <AiOutlineRise />,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu.",
+  },
+  {
+    name: APP_ROUTES_LABEL.data_quality,
+    id: APP_ROUTES.data_quality,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.data_quality}`,
+    icon: () => <AiOutlineBarChart />,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu.",
+  },
+  {
+    name: APP_ROUTES_LABEL.unmet_need_definition,
+    id: APP_ROUTES.unmet_need_definition,
+    route: `${APP_ROUTES.outputs}/${APP_ROUTES.unmet_need_definition}`,
+    icon: () => <AiOutlineInfo />,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu.",
+  },
+  {
+    name: APP_ROUTES_LABEL.help,
+    id: APP_ROUTES.help,
+    route: APP_ROUTES.help,
+    icon: () => <AiOutlineQuestionCircle />,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor odio, lobortis in ornare et, cursus varius arcu."
+  },
+]
 
 const Home = () => {
   const [open, setOpen] = useState(false);
@@ -37,6 +111,20 @@ const Home = () => {
         </>
       ) : (
         <>
+        <div className="grid mb-8 gap-[0.5rem] grid-cols-4">
+          {NavigationMenu.map(item => {
+            return (
+              <div
+                key={item.id}
+                onClick={() => window.location.href = item.route}
+                className={`px-2 hover:shadow-lg gap-1 font-[600] transition-all ease-in-out duration-200 shadow-md cursor-pointer rounded bg-slate-200 py-4 grid place-content-center text-center`}
+              >
+                {item.name}
+                <div className="text-xs font-[400]">{item.description}</div>
+              </div>
+            )
+          })}
+        </div>
           <RealTimeBox />
           <HCPTable />
         </>
